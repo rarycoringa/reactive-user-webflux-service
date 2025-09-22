0eb73fe2dcf13aed3dc970a17fdb57a4b04908b5
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-09-22 18:09:22 -0300

    enable webflux on pom.xml and main app class

--- src/main/java/br/edu/ufrn/ingestion/IngestionApplication.java
+++ src/main/java/br/edu/ufrn/ingestion/IngestionApplication.java
@@ -2,8 +2,10 @@ package br.edu.ufrn.ingestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class IngestionApplication {
 
 	public static void main(String[] args) {
